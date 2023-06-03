dfdde68c14e196306d696608e27b1e9cf92d3e9c
keylou <116100969+keylou@users.noreply.github.com>
2023-06-03 10:17:12 +0300

    Create 1.sh

---
+++ OS1/1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+find ~ -maxdepth 1 -name ".*" -type d | wc -l
+#!/bin/bash
+
+cp a /usr/bin
+chmod g+x /usr/bin/a
+
+cp b.lib /usr/lib/leo
+
+mkdir /usr/share/doc/leo
+cp c.doc/share/leo
